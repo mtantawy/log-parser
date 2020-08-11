a4b7878d957e14146d0cb06682ed7224e9030d85
Mahmoud Tantawy <me@mtantawy.com>
2020-08-11 22:28:40 +0200

    Let's make ourselves a JAR!

--- src/com/mtantawy/logparser/Main.java
+++ src/com/mtantawy/logparser/Main.java
@@ -1,8 +1,13 @@
 package com.mtantawy.logparser;
 
+import com.mtantawy.logparser.kodi.Parser;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Parser parser = new Parser();
+        String logLine = "2020-08-02 19:22:27.057 T:1899965712  NOTICE: VideoPlayer: finished waiting";
+        String parsed = parser.parseLine(logLine);
+        System.out.println(parsed);
     }
 }
