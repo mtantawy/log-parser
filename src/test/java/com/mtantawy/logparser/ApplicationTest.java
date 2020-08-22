a2c1948da6464cb50b2abd6cc89765ddfa7fac65
Mahmoud Tantawy <me@mtantawy.com>
2020-08-22 16:13:47 +0200

    ApplicationTest

--- src/test/java/com/mtantawy/logparser/ApplicationTest.java
+++ src/test/java/com/mtantawy/logparser/ApplicationTest.java
@@ -1,12 +1,16 @@
 package com.mtantawy.logparser;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest {
+    @Test
+    public void ExceptionThrownForNonExistingFile() {
+        Exception exception = Assertions.assertThrows(
+                Exception.class,
+                () -> new Application("Kodi", "non-existing-file")
+        );
 
-    @BeforeEach
-    void setUp() {
+        Assertions.assertEquals("File does not exist or not readable!", exception.getMessage());
     }
 }
