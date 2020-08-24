60cb5b26eb32a5675cdb5a38cee4c261e720dfef
Mahmoud Tantawy <me@mtantawy.com>
2020-08-24 21:44:21 +0200

    Handle parse errors in KodiParser

--- src/main/java/com/mtantawy/logparser/kodi/KodiParser.java
+++ src/main/java/com/mtantawy/logparser/kodi/KodiParser.java
@@ -2,6 +2,7 @@ package com.mtantawy.logparser.kodi;
 
 import com.mtantawy.logparser.Parser;
 
+import java.time.format.DateTimeParseException;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,21 +13,24 @@ public class KodiParser implements Parser {
 
     @Override
     public Optional<LogLine> parseLine(String line) {
-        // remove all invisible control characters
-        // http://www.regular-expressions.info/unicode.html
-        line  = line.replaceAll("[\\p{C}]", "");
         Pattern pattern = Pattern.compile(PATTERN);
         Matcher matcher = pattern.matcher(line);
         if (!matcher.matches()) {
-            System.out.println("found nothing! :O");
-            System.out.printf("Line: %s", line);
+            System.out.println("No matches found!");
+            System.out.printf("Line: %s\n", line);
             return Optional.empty();
         }
 
         LogLine logLine = new LogLine();
         logLine.setSourceLine(matcher.group(0));
-        logLine.setTimestamp(matcher.group(1) + "T" + matcher.group(3));
-        logLine.setThreadId(matcher.group(5).split(":")[1]);
+        try {
+            logLine.setTimestamp(matcher.group(1) + "T" + matcher.group(3));
+        } catch (DateTimeParseException exception) {
+            System.out.println("Failed to parse Timestamp: " + matcher.group(1) + "T" + matcher.group(3));
+            return Optional.empty();
+        }
+        String[] threadInfo = matcher.group(5).split(":");
+        logLine.setThreadId((threadInfo.length == 2) ? threadInfo[1] : "0");
         logLine.setLevel(matcher.group(7).split(":")[0]);
         logLine.setMessage(matcher.group(9));
 
